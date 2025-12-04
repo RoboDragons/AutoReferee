5ea42a8f8e7b87ed799345d18ffb1328940aab00
keishin32 <michuanjingxin@gmail.com>
2025-12-04 13:06:20 +0900

    Update BallSpeedModel.java

--- modules/autoreferee-gui/src/main/java/edu/tigers/autoref/model/ballspeed/BallSpeedModel.java
+++ modules/autoreferee-gui/src/main/java/edu/tigers/autoref/model/ballspeed/BallSpeedModel.java
@@ -14,73 +14,97 @@ import edu.tigers.sumatra.wp.data.WorldFrameWrapper;
  */
 public class BallSpeedModel
 {
-	private double lastBallSpeed = 0.0d;
-	private double lastEstimatedBallSpeed = 0.0d;
+    private double lastBallSpeed = 0.0d;
+    private double lastEstimatedBallSpeed = 0.0d;
 
-	private GameState lastState = GameState.HALT;
-	private boolean gameStateChanged = false;
+    private GameState lastState = GameState.HALT;
+    private boolean gameStateChanged = false;
 
-
-	/**
-	 * @param wFrameWrapper
-	 */
-	public void update(final WorldFrameWrapper wFrameWrapper)
-	{
-		GameState curState = wFrameWrapper.getGameState();
-
-		if (!curState.equals(lastState))
-		{
-			gameStateChanged = true;
-		}
-
-		lastBallSpeed = wFrameWrapper.getSimpleWorldFrame().getBall().getVel3().getLength();
-		lastEstimatedBallSpeed = wFrameWrapper.getSimpleWorldFrame().getKickedBall()
-				.map(KickedBall::getKickVel)
-				.map(IVector3::getLength).orElse(0.0);
-
-		lastState = curState;
-
-	}
+    // 前フレームで KickedBall が存在したかどうか（キック検出用）
+    private boolean hadKickedBallLastFrame = false;
 
 
-	/**
-	 *
-	 */
-	public void reset()
-	{
-		gameStateChanged = false;
-	}
+    /**
+     * @param wFrameWrapper
+     */
+    public void update(final WorldFrameWrapper wFrameWrapper)
+    {
+        GameState curState = wFrameWrapper.getGameState();
+
+        if (!curState.equals(lastState))
+        {
+            gameStateChanged = true;
+        }
+
+        // 現在のボール速度
+        lastBallSpeed = wFrameWrapper.getSimpleWorldFrame().getBall().getVel3().getLength();
+
+        var swf = wFrameWrapper.getSimpleWorldFrame();
+
+        boolean hasKickedBall = swf.getKickedBall().isPresent();
+
+        if (hasKickedBall && !hadKickedBallLastFrame)
+        {
+            // 新しいキックを検出（立ち上がりエッジ）
+            KickedBall kickedBall = swf.getKickedBall().get();
+            lastEstimatedBallSpeed = kickedBall.getKickVel().getLength();
+
+            // ここで1キックにつき1回のみ出力
+            System.out.println("Estimated Kick Initial Speed: " + lastEstimatedBallSpeed);
+        }
+        else if (!hasKickedBall)
+        {
+            // lastEstimatedBallSpeed = 0.0;
+        }
+
+        // 次フレーム用に記録
+        hadKickedBallLastFrame = hasKickedBall;
+        // ==========================================================
 
 
-	/**
-	 * @return
-	 */
-	public double getLastBallSpeed()
-	{
-		return lastBallSpeed;
-	}
+        lastState = curState;
+    }
 
 
-	/**
-	 * @return
-	 */
-	public GameState getLastState()
-	{
-		return lastState;
-	}
+    /**
+     *
+     */
+    public void reset()
+    {
+        gameStateChanged = false;
+    }
 
 
-	/**
-	 * @return
-	 */
-	public boolean hasGameStateChanged()
-	{
-		return gameStateChanged;
-	}
+    /**
+     * @return
+     */
+    public double getLastBallSpeed()
+    {
+        return lastBallSpeed;
+    }
 
 
-	public double getLastEstimatedBallSpeed()
-	{
-		return lastEstimatedBallSpeed;
-	}
+    /**
+     * @return
+     */
+    public GameState getLastState()
+    {
+        return lastState;
+    }
+
+
+    /**
+     * @return
+     */
+    public boolean hasGameStateChanged()
+    {
+        return gameStateChanged;
+    }
+
+
+    public double getLastEstimatedBallSpeed()
+    {
+        return lastEstimatedBallSpeed;
+    }
 }
+
